b201c9d0f107afca98e619ad06ff8974052db0df
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-04-30 07:38:58 +0545

    Himalayan Market Initial Set up

---
+++ root/webparent/src/main/java/com/himalaya/market/WebParentApplication.java
@@ -0,0 +1,13 @@
+package com.himalaya.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebParentApplication.class, args);
+    }
+
+}
